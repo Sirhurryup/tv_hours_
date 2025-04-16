65e6f556fa78e361d1d7ad752fe14731b538da06
Sirhurryup <dotcolumbia@gmail.com>
2025-04-16 15:07:23 -0400

    Analysis_first_run

---
+++ main.R
@@ -0,0 +1,11 @@
+pacman::p_load(here, tidyverse)
+
+
+tv_hours_table <- gss_cat %>% 
+  group_by(marital) %>% 
+  summarise(mean_tv_hours = mean(tvhours, na.rm = TRUE))
+
+
+write_csv(tv_hours_table, here("tv_hours_by_marital.csv"))
+
+
